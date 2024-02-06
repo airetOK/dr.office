708bf8087a9d8acf8087fae6f309d4b0f6a31566
airetOK <sergey.bezrukavyy@gmail.com>
2024-02-06 16:57:58 +0200

    add appending patient logic

--- init.py
+++ init.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template
-from repository.patients_repository import get_patients
+from flask import Flask, render_template, redirect, request
+import repository.patients_repository as pr
 
 app = Flask(__name__)
 
 @app.route("/")
 def office():
-    return render_template('office.html', patients=get_patients())
+    return render_template('office.html', patients=pr.get_patients())
+
+
+@app.route("/add", methods=['POST'])
+def add_patient():
+    pr.add_patient(request.form)
+    return redirect('/')
--- repository/patients_repository.py
+++ repository/patients_repository.py
@@ -1,36 +1,49 @@
+from werkzeug.datastructures import ImmutableMultiDict
+from datetime import datetime
+
 # mock data
 patients = [
     {'fullname': 'Безрукавий Сергій',
      'teeth': '16,17',
-     'action': 'реставрація, консультація',
+     'actions': 'реставрація, консультація',
      'price': 100.50,
      'date': '2024-02-05'},
      {'fullname': 'Пригара Іван',
      'teeth': '46',
-     'action': 'ендо',
+     'actions': 'ендо',
      'price': 150,
      'date': '2024-02-06'},
      {'fullname': 'Шевцов Богдан',
      'teeth': '',
-     'action': 'чистка',
+     'actions': 'чистка',
      'price': 70,
      'date': '2024-02-06'},
      {'fullname': 'Микита Валерія',
      'teeth': '21,22',
-     'action': 'реставрація',
+     'actions': 'реставрація',
      'price': 180,
      'date': '2024-02-11'},
      {'fullname': 'Шетеля Володимир',
      'teeth': '23',
-     'action': 'ендо',
+     'actions': 'ендо',
      'price': 120,
      'date': '2024-02-11'},
      {'fullname': 'Форос Анатолій',
      'teeth': '34',
-     'action': 'ендо',
+     'actions': 'ендо',
      'price': 150,
      'date': '2024-02-12'}
 ]
 
+def add_patient(form: ImmutableMultiDict) -> None:
+    patients.append({'fullname': form['fullName'],
+                     'teeth': form['teeth'],
+                     'actions': form['actions'],
+                     'price': form['price'],
+                     'date': __get_current_date()})
+
 def get_patients() -> list[object]:
     return patients
+
+def __get_current_date() -> str:
+    return datetime.today().strftime('%Y-%m-%d')
