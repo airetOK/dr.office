f539d52ea7526965c65b53089f3606d37a3c7113
airetOK <sergey.bezrukavyy@gmail.com>
2024-02-08 19:35:13 +0200

    Replace postgres to sqlite

--- repository/patients_repository.py
+++ repository/patients_repository.py
@@ -1,21 +1,20 @@
 from werkzeug.datastructures import ImmutableMultiDict
 from datetime import datetime
-import psycopg2
-from configuration.config import load_config
+import sqlite3
 
 
 def connect():
     try:
-        with psycopg2.connect(**load_config()) as conn:
+        with sqlite3.connect("patients.db") as conn:
             return conn
-    except (psycopg2.DatabaseError, Exception) as error:
+    except (Exception) as error:
         print(error)
+    return conn
 
 def add_patient(form: ImmutableMultiDict) -> None:
     conn = connect()
     cur = conn.cursor()
-    cur.execute('INSERT INTO public."patients"("fullName", teeth, actions, price, date) VALUES (%s, %s, %s, %s, %s)', 
-                (form['fullName'], form['teeth'], form['actions'], form['price'], __get_current_date()))
+    cur.execute(f"INSERT INTO patients(fullName, teeth, actions, price, date) VALUES ('{form['fullName']}', '{form['teeth']}', '{form['actions']}', '{form['price']}', '{__get_current_date()}')")
     conn.commit()
     cur.close()
     conn.close()
@@ -23,7 +22,7 @@ def add_patient(form: ImmutableMultiDict) -> None:
 def get_patients() -> list[object]:
     conn = connect()
     cur = conn.cursor()
-    cur.execute('SELECT id, "fullName", teeth, actions, price, date FROM public."patients"')
+    cur.execute('SELECT id, fullName, teeth, actions, price, date FROM patients')
     patients = __convert(cur.fetchall())
     cur.close()
     conn.close()
