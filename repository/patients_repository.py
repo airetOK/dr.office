d62811016fee4fee50f825eeeeeb0b2dfec6eee1
airetOK <sergey.bezrukavyy@gmail.com>
2024-11-06 12:48:20 +0200

    Add column phone

--- repository/patients_repository.py
+++ repository/patients_repository.py
@@ -12,7 +12,7 @@ LIMIT = 10
 def __connect(path_to_db: str):
     with sqlite3.connect(path_to_db) as conn:
         cur = conn.cursor()
-        cur.execute('CREATE TABLE IF NOT EXISTS patients(id INTEGER PRIMARY KEY, fullName NOT NULL, teeth, actions, price, comment, language, date, user_id)')
+        cur.execute('CREATE TABLE IF NOT EXISTS patients(id INTEGER PRIMARY KEY, fullName NOT NULL, teeth, actions, price, comment, language, phone, date, user_id)')
         conn.commit()
         return conn
 
@@ -22,9 +22,10 @@ def add_patient(form: ImmutableMultiDict, user_id: int) -> None:
         conn = __connect(os.getenv('DB_PATH'))
         cur = conn.cursor()
         cur.execute(f'''INSERT INTO 
-                    patients(fullName, teeth, actions, price, comment, language, date, user_id) 
+                    patients(fullName, teeth, actions, price, comment, language, phone, date, user_id) 
                     VALUES ('{form['fullName']}', '{form['teeth']}', '{form['actions']}', 
-                    '{form['price']}', '{form['comment']}', '{form['language']}', '{form['date']}', {user_id})''')
+                    '{form['price']}', '{form['comment']}', '{form['language']}', 
+                    '{form['phone']}', '{form['date']}', {user_id})''')
         conn.commit()
         logger.info(f"The doctor with id {str(user_id)} save patient successfully")
     except (Exception) as error:
@@ -41,7 +42,8 @@ def update_patient(form: ImmutableMultiDict, id: str, user_id: int) -> None:
         cur = conn.cursor()
         cur.execute(f'''UPDATE patients 
                     SET fullname='{form['fullName']}', teeth='{form['teeth']}', actions='{form['actions']}',
-                      price='{form['price']}', comment='{form['comment']}', language='{form['language']}', date='{form['date']}' 
+                      price='{form['price']}', comment='{form['comment']}', language='{form['language']}', 
+                      phone='{form['phone']}', date='{form['date']}' 
                     WHERE id = {id} and user_id = {user_id}''')
         conn.commit()
         logger.info(f"The doctor with id {str(user_id)} update patient successfully")
@@ -74,7 +76,7 @@ def get_patients(user_id: int, skip: str) -> list[object]:
         conn = __connect(os.getenv('DB_PATH'))
         cur = conn.cursor()
         cur.execute(f'''SELECT DISTINCT
-                    p.id, p.fullName, p.teeth, p.actions, p.price, p.comment, p.language, p.date 
+                    p.id, p.fullName, p.teeth, p.actions, p.price, p.comment, p.language, p.phone, p.date 
                     FROM patients p 
                     JOIN users u ON p.user_id={user_id}
                     ORDER BY p.id 
@@ -97,7 +99,7 @@ def get_patients_by_full_name(full_name, skip, user_id: int) -> list[object]:
     try:
         conn = __connect(os.getenv('DB_PATH'))
         cur = conn.cursor()
-        cur.execute(f'''SELECT DISTINCT p.id, p.fullName, p.teeth, p.actions, p.price, p.comment, p.language, p.date 
+        cur.execute(f'''SELECT DISTINCT p.id, p.fullName, p.teeth, p.actions, p.price, p.comment, p.language, p.phone, p.date 
                     FROM patients p
                     JOIN users u ON p.user_id={user_id}
                     WHERE p.fullName LIKE \'%{full_name}%\' 
@@ -119,7 +121,7 @@ def get_patient(id, user_id: int) -> object:
     try:
         conn = __connect(os.getenv('DB_PATH'))
         cur = conn.cursor()
-        cur.execute(f'''SELECT p.id, p.fullName, p.teeth, p.actions, p.price, p.comment, p.language, p.date 
+        cur.execute(f'''SELECT p.id, p.fullName, p.teeth, p.actions, p.price, p.comment, p.language, p.phone, p.date 
                     FROM patients p
                     JOIN users u ON p.user_id={user_id}
                     WHERE p.id = {id}''')
@@ -179,7 +181,7 @@ def get_patients_by_actions(actions: str, skip: int, user_id: int) -> list[objec
     try:
         conn = __connect(os.getenv('DB_PATH'))
         cur = conn.cursor()
-        cur.execute(f'''SELECT DISTINCT p.id, p.fullName, p.teeth, p.actions, p.price, p.comment, p.language, p.date 
+        cur.execute(f'''SELECT DISTINCT p.id, p.fullName, p.teeth, p.actions, p.price, p.comment, p.language, p.phone, p.date 
                     FROM patients p
                     JOIN users u ON p.user_id={user_id}
                     WHERE p.actions LIKE \'%{actions}%\' 
@@ -225,7 +227,7 @@ def get_patients_by_actions_count(actions, user_id: int) -> int:
 
 def __convert(db_list) -> list[object]:
     patients = []
-    for id, fullName, teeth, actions, price, comment, language, date in db_list:
+    for id, fullName, teeth, actions, price, comment, language, phone, date in db_list:
         patients.append({'id': id,
                          'fullname': fullName,
                          'teeth': teeth,
@@ -234,5 +236,6 @@ def __convert(db_list) -> list[object]:
                          'comment': comment,
                          'language': language,
                          'lang_svg': get_svg_name_by_language(language),
+                         'phone': phone,
                          'date': date})
     return patients
--- sql/init_db.py
+++ sql/init_db.py
@@ -5,7 +5,7 @@ query = '''DROP TABLE IF EXISTS users;
     DROP TABLE IF EXISTS patients;
     CREATE TABLE IF NOT EXISTS 
     patients(id INTEGER PRIMARY KEY, fullName NOT NULL, teeth, actions, price, 
-    comment, language, date, user_id INTEGER REFERENCES users(id));'''
+    comment, language, phone, date, user_id INTEGER REFERENCES users(id));'''
 
 with sqlite3.connect('patients.db') as conn:
     cur = conn.cursor()
